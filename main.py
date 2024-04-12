41493749b5cb2ba92c2db5e3b743782715f78b0d
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-04-12 14:21:34 +0300

    Implement the first task based on the example

---
+++ main.py
@@ -0,0 +1,18 @@
+import datetime
+
+
+def get_days_from_today(date: str) -> int:
+    '''
+    Calculate the number of days between the current date and the specified date.
+
+    :param date: string
+
+    :return: integer
+    '''
+    try:
+        given: datetime = datetime.datetime.strptime(date, '%Y-%m-%d')
+    except Exception:
+        print('The "date" argument must match this pattern: "YYYY-MM-DD"!')
+        return None
+    
+    return (given.date() - datetime.date.today()).days
